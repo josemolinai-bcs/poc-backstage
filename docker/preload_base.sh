3ec97b37c5e332eab6f3d830afe5c1d5bf8fc496
José Molina <jose.molina.i@gmail.com>
2022-10-18 21:11:23 -0300

    nfinx controller

--- docker/preload_base.sh
+++ docker/preload_base.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 ./preload.sh kindest/node:v1.24.3 kindest/node:v1.24.3
+./preload.sh registry.k8s.io/ingress-nginx/controller:v1.2.1 ingress-nginx/controller:v1.2.1
+./preload.sh registry.k8s.io/ingress-nginx/kube-webhook-certgen:v1.1.1 ingress-nginx/kube-webhook-certgen:v1.1.1
